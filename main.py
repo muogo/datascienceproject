a74873db6037bc5cce29a4a293128afdb1dc4fd4
amar <amar@gmail.com>
2025-01-22 21:12:07 +0800

    model trani implementasion

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.datascienca import logger
 from src.datascienca.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascienca.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.datascienca.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
+from src.datascienca.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -33,6 +34,17 @@ try:
    logger.info(f">>>>>>>>>> {STAGE_NAME} started <<<<<<<<<<")
    data_ingestion = DataTransformationTrainingPipeline()
    data_ingestion.initiate_data_transformation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+   logger.exception(e)
+   raise e
+
+
+STAGE_NAME = "Model trainer stage"
+try:
+   logger.info(f">>>>>>>>>> {STAGE_NAME} started <<<<<<<<<<")
+   data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.initiate_model_training()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
    logger.exception(e)
